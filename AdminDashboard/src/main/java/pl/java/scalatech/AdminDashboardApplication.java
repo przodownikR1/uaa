63b5c03d3380f8e50f962b3c60cb705d62e39d9b
przodownikR1 <przodownikR1@gmail.com>
2017-03-21 12:34:33 +0100

    Update AdminDashboardApplication.java

--- AdminDashboard/src/main/java/pl/java/scalatech/AdminDashboardApplication.java
+++ AdminDashboard/src/main/java/pl/java/scalatech/AdminDashboardApplication.java
@@ -1,11 +1,21 @@
 package pl.java.scalatech;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
+import de.codecentric.boot.admin.notify.LoggingNotifier;
+import de.codecentric.boot.admin.notify.Notifier;
+import de.codecentric.boot.admin.notify.RemindingNotifier;
+import de.codecentric.boot.admin.notify.filter.FilteringNotifier;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -20,4 +30,29 @@ public class AdminDashboardApplication {
         springApplication.addListeners(new ApplicationPidFileWriter());
         springApplication.run(args);
     }
+	@Configuration
+    public static class NotifierConfig {
+    @Bean
+    @Primary
+    public RemindingNotifier remindingNotifier() {
+        RemindingNotifier notifier = new RemindingNotifier(filteringNotifier(loggerNotifier()));
+        notifier.setReminderPeriod(TimeUnit.SECONDS.toMillis(10));
+        return notifier;
+    }
+
+    @Scheduled(fixedRate = 1_000L)
+    public void remind() {
+        remindingNotifier().sendReminders();
+    }
+
+    @Bean
+    public FilteringNotifier filteringNotifier(Notifier delegate) {
+        return new FilteringNotifier(delegate);
+    }
+
+    @Bean
+    public LoggingNotifier loggerNotifier() {
+        return new LoggingNotifier();
+    }
+}
 }
