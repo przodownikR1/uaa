1dd99578cc5115d6541625e93fa99dd5eb253cd3
przodownikR1 <przodownikR1@gmail.com>
2017-03-21 11:26:23 +0100

    Update ConfigSeverConfig.java

--- ConfigServer_UAA/src/main/java/pl/java/scalatech/ConfigSeverConfig.java
+++ ConfigServer_UAA/src/main/java/pl/java/scalatech/ConfigSeverConfig.java
@@ -1,18 +1,13 @@
 package pl.java.scalatech;
-
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
 @EnableConfigServer
 @EnableDiscoveryClient
 @EnableEurekaClient
-@EnableCircuitBreaker
-@EnableRetry
 public class ConfigSeverConfig {
 
 }
