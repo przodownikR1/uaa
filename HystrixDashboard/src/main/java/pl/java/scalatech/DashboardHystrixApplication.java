80eedc4eb885e093d7f4ca3b79bf2e3ac15cd715
przodownikR1 <przodownikR1@gmail.com>
2017-03-23 16:19:23 +0100

    Update DashboardHystrixApplication.java

--- HystrixDashboard/src/main/java/pl/java/scalatech/DashboardHystrixApplication.java
+++ HystrixDashboard/src/main/java/pl/java/scalatech/DashboardHystrixApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
@@ -25,6 +26,7 @@ public class DashboardHystrixApplication {
     }
     
     @Bean
+    @LoadBalanced
     RestTemplate restTemplate(){
         return new RestTemplate();
     }
