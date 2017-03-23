5360cef1a9b9f15beb11d6ef62cad7ba665859b9
przodownikR1 <przodownikR1@gmail.com>
2017-03-23 16:18:42 +0100

    Create MetricsController.java

---
+++ HystrixDashboard/src/main/java/pl/java/scalatech/MetricsController.java
@@ -0,0 +1,39 @@
+package pl.java.scalatech;
+
+import java.util.Map;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.common.collect.Maps;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class MetricsController {
+
+    private final DiscoveryClient eurekaClient;
+
+    private final RestTemplate restTemplate;
+
+    @GetMapping("/metricTest")
+    public Map<String, Object> getMetrics() {
+
+        eurekaClient.getServices().forEach(service -> {
+            String url = "http://" + service + "/metrics";
+            log.error(" @@@@@@  {} , - > {}                 ",service,url);
+            
+            
+           Map<String, Object> metrics = restTemplate.getForObject(url, Map.class);
+           log.error("metrics : {} ", metrics);
+        });
+        return Maps.newHashMap();
+
+    }
+
+}
