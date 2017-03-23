02c15434c5c1dcbf7fd576ff7e4cfd15b9b5cbaf
przodownikR1 <przodownikR1@gmail.com>
2017-03-23 17:04:43 +0100

    Create GreetingFeignResource.java

---
+++ UserService/src/main/java/pl/java/scalatech/config/GreetingFeignResource.java
@@ -0,0 +1,25 @@
+package pl.java.scalatech.config;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import feign.Headers;
+@FeignClient(name="greeting")
+@Component
+public interface GreetingFeignResource {
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/message/sean")
+    String getMessageNoName();
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/message/{name}")
+    String getMessage(@PathVariable("name") String name);
+
+    @Headers("Content-Type: application/json")
+    @RequestMapping(method = RequestMethod.POST, value = "/message/{newGreeting}")
+    void updateMessage(@PathVariable("newGreeting") String message);
+    
+}
+//client -> greeting.ribbon.listOfServers: localhost:9000
