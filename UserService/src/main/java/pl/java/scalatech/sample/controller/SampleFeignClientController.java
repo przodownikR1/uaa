f7a7f3d5c0d2a5966732be47dd4d2c3721f03ad0
przodownikR1 <przodownikR1@gmail.com>
2017-03-23 17:05:19 +0100

    Update SampleFeignClientController.java

--- UserService/src/main/java/pl/java/scalatech/sample/controller/SampleFeignClientController.java
+++ UserService/src/main/java/pl/java/scalatech/sample/controller/SampleFeignClientController.java
@@ -1,19 +1,23 @@
 package pl.java.scalatech.sample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
+import pl.java.scalatech.config.GreetingFeignResource;
 
 @RestController
-@RequiredArgsConstructor
 public class SampleFeignClientController {
-    
+    @Autowired
     private GreetingFeignResource greetingFeignResource;
     
     @GetMapping("/feignTest/{name}")
     String getTest(@PathVariable String name){
       return greetingFeignResource.getMessage(name);   
+    }
+    @GetMapping("/feignTest2/{name}")
+    String getTest2(){
+      return greetingFeignResource.getMessageNoName();   
     }
 }
