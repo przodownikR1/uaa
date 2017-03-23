8dc58d8517ddc5f43963b4f1fe3e83ce2bc5b149
przodownikR1 <przodownikR1@gmail.com>
2017-03-23 16:39:04 +0100

    Update ComputeController.java

--- HystrixDashboard/src/main/java/pl/java/scalatech/ComputeController.java
+++ HystrixDashboard/src/main/java/pl/java/scalatech/ComputeController.java
@@ -1,21 +1,32 @@
-@RestController
-    @RequestMapping("/")
-    public class ComputeController {
-    	
-    	private String welcome = "Welcome to Feign"; 
+package pl.java.scalatech;
 
-        @GetMapping("/message/{name}")
-        public String add(@PathVariable(value="name") String name) {
-            
-            logger.info("Received request from {} for a greeting.", name);
-            
-            return welcome + " " + name;
-        }
-        
-        @PostMapping("/message/{greeting}") 
-        public void updateGreeting(@PathVariable(value="greeting") String greeting) {
-        	welcome = greeting;
-        	
-        	logger.info("Greeting update to {}", welcome);
-        }
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/")
+@Slf4j
+public class ComputeController {
+
+    private String welcome = "Welcome to Feign";
+
+    @GetMapping("/message/{name}")
+    public String add(@PathVariable(value = "name") String name) {
+
+        log.info("Received request from {} for a greeting.", name);
+
+        return welcome + " " + name;
+    }
+
+    @PostMapping("/message/{greeting}")
+    public void updateGreeting(@PathVariable(value = "greeting") String greeting) {
+        welcome = greeting;
+
+        log.info("Greeting update to {}", welcome);
+    }
 }
