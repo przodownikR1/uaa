4cdb95172c90f324c7039a5a0768ec535f69c80e
przodownikR1 <przodownikR1@gmail.com>
2017-03-24 15:44:33 +0100

    Create CarRestRepository.java

---
+++ CarService/src/main/java/pl/java/scalatech/sample/repo/CarRestRepository.java
@@ -0,0 +1,10 @@
+package pl.java.scalatech.sample.repo;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import pl.java.scalatech.sample.domain.Car;
+
+@RepositoryRestResource(collectionResourceRel = "car", path = "v1")
+interface CarRestRepository extends PagingAndSortingRepository<Car, Long> {
+
