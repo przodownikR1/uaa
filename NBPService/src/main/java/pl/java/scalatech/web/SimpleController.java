2187077df46f0bcd008109ef4509d6781a241069
Slawomir Borowiec <przodownik@tlen.pl>
2017-03-25 08:50:58 +0100

    nbp service eliminate some errors

--- NBPService/src/main/java/pl/java/scalatech/web/SimpleController.java
+++ NBPService/src/main/java/pl/java/scalatech/web/SimpleController.java
@@ -1,3 +1,12 @@
+package pl.java.scalatech.web;
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/nbp")
 @RequiredArgsConstructor
