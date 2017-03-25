4833c76818238f3375920a0fd01f7e348b59837c
Slawomir Borowiec <przodownik@tlen.pl>
2017-03-25 19:44:32 +0100

    eureka lookup strategy changed -> properties files

--- GatewayAPI/src/main/java/pl/java/scalatech/GatewayAPIApplication.java
+++ GatewayAPI/src/main/java/pl/java/scalatech/GatewayAPIApplication.java
@@ -43,4 +43,5 @@ public class GatewayAPIApplication {
             }
         };
     }
+    
 }
--- GatewayAPI/src/main/java/pl/java/scalatech/config/GatewayConfig.java
+++ GatewayAPI/src/main/java/pl/java/scalatech/config/GatewayConfig.java
@@ -1,12 +1,80 @@
 package pl.java.scalatech.config;
 
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.Filter;
+
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import pl.java.scalatech.filter.SimpleFilter;
 
 
 @Configuration
-@Profile(ProfileApp.PROFILE)
 public class GatewayConfig {
+	@Bean
+    Filter shallowEtagHeaderFilter() {
+       return new ShallowEtagHeaderFilter();
+   }
+   
+   @Bean
+    SimpleFilter simpleFilter() {
+     return new SimpleFilter();
+   }
+   
+   @Bean
+    ZuulFallbackProvider zuulFallbackProvider() {
+       return new ZuulFallbackProvider() {
+           @Override
+           public String getRoute() {
+               return "customer";
+           }
 
+           @Override
+           public ClientHttpResponse fallbackResponse() {
+               return new ClientHttpResponse() {
+                   @Override
+                   public HttpStatus getStatusCode() throws IOException {
+                       return HttpStatus.OK;
+                   }
+
+                   @Override
+                   public int getRawStatusCode() throws IOException {
+                       return 200;
+                   }
+
+                   @Override
+                   public String getStatusText() throws IOException {
+                       return "OK";
+                   }
+
+                   @Override
+                   public void close() {
+
+                   }
+
+                   @Override
+                   public InputStream getBody() throws IOException {
+                       return new ByteArrayInputStream("fallback".getBytes());
+                   }
+
+                   @Override
+                   public HttpHeaders getHeaders() {
+                       HttpHeaders headers = new HttpHeaders();
+                       headers.setContentType(MediaType.APPLICATION_JSON);
+                       return headers;
+                   }
+               };
+           }
+       };
+}
 }
