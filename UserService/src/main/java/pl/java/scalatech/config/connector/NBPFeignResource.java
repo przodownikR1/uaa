742173b79a07f61c61c750c3fc4708e02dfddca8
Slawomir Borowiec <przodownik@tlen.pl>
2017-03-25 22:05:51 +0100

    polish code

--- UserService/src/main/java/pl/java/scalatech/config/connector/NBPFeignResource.java
+++ UserService/src/main/java/pl/java/scalatech/config/connector/NBPFeignResource.java
@@ -2,18 +2,18 @@ package pl.java.scalatech.config.connector;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(name="nbp-service")
 @Component
 public interface NBPFeignResource {
     
     
-	@RequestMapping("/byCode/{code}")
+	 @RequestMapping(value="/byCode/{code}",method = RequestMethod.GET)
     String getMutlipierByCode(@PathVariable String code);
 
-	  @GetMapping("/simple")
+	  @RequestMapping(method = RequestMethod.GET, value="/simple")
 	  String getMessage();
     
     
