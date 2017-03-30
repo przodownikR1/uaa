f81655018a263ee337da9d8e22c84ea8e79d966e
przodownikR1 <przodownikR1@gmail.com>
2017-03-30 17:02:30 +0200

    Update CurrencyController.java

--- nbp-service/src/main/java/pl/java/scalatech/web/CurrencyController.java
+++ nbp-service/src/main/java/pl/java/scalatech/web/CurrencyController.java
@@ -1,21 +1,26 @@
 package pl.java.scalatech.web;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.samsung.microservice.poc.camel.nbp.NbpCurrentExchange;
+
 import lombok.RequiredArgsConstructor;
-import pl.java.scalatech.camel.nbp.NbpCurrentExchange;
 
 @RestController
 @RequiredArgsConstructor
 class CurrencyController {
-    
+
     private final NbpCurrentExchange ce;
-    
+    private final DiscoveryClient client;
+
     @GetMapping("/byCode/{code}")
-    public String getMutlipierByCode(@PathVariable String code){
-        return ce.getCurrentMultiplier(code);
+    public String getMutlipierByCode(@PathVariable String code) {
+        ServiceInstance localInstance = client.getLocalServiceInstance();
+        return ce.getCurrentMultiplier(code) + " " + localInstance.getServiceId() + ":" + localInstance.getHost() + ":" + localInstance.getPort();
     }
-    
+
 }
