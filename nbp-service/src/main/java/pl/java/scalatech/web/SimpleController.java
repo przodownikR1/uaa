f028cfa5f2476c0776ab3ce66ec6e7d5ed6b738a
przodownikR1 <przodownikR1@gmail.com>
2017-03-31 10:29:30 +0200

    Update SimpleController.java

--- nbp-service/src/main/java/pl/java/scalatech/web/SimpleController.java
+++ nbp-service/src/main/java/pl/java/scalatech/web/SimpleController.java
@@ -1,5 +1,7 @@
 package pl.java.scalatech.web;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +12,19 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class SimpleController {
-   
+
     private final Environment environment;
+    private final DiscoveryClient client;
 
     @GetMapping("/simple")
-    String getMessage(){
-        return "sample message from  NBP : port : " +  environment.getProperty("server.port"); 
+    String getMessage() {
+        return "sample message from  NBP : port : " + environment.getProperty("server.port");
     }
-    
+
+    @RequestMapping("/hello")
+    public String hello() {
+        ServiceInstance localInstance = client.getLocalServiceInstance();
+        return "Hello World: " + localInstance.getServiceId() + ":" + localInstance.getHost() + ":" + localInstance.getPort();
+    }
+
 }
