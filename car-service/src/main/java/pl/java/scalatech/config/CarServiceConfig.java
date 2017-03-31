f4a946adc6f84b1e5e6e055277da6c578aa87db6
przodownikR1 <przodownikR1@gmail.com>
2017-03-31 10:26:31 +0200

    Update CarServiceConfig.java

--- car-service/src/main/java/pl/java/scalatech/config/CarServiceConfig.java
+++ car-service/src/main/java/pl/java/scalatech/config/CarServiceConfig.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.RequiredArgsConstructor;
@@ -15,24 +18,27 @@ import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableAsync
+@EnableCircuitBreaker
+@EnableEurekaClient
 @Slf4j
 class CarServiceConfig {
 
-	@Bean
-	@LoadBalanced
-	RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-	
-	  private final DiscoveryClient discoveryClient;
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-	    @Bean
-	    CommandLineRunner commandLineRunner() {
-	        return (args) -> {
-	            List<ServiceInstance> instances = discoveryClient.getInstances("discovery-service");
-	            if (instances.size() > 0) {
-	                log.info("eureka instances :  {}",instances.get(0).getUri().toString());
-	            }
-	        };
-	    }
+    private final DiscoveryClient discoveryClient;
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return (args) -> {
+            List<ServiceInstance> instances = discoveryClient.getInstances("discovery-service");
+            if (instances.size() > 0) {
+                log.info("eureka instances :  {}", instances.get(0).getUri().toString());
+            }
+        };
+    }
 }
